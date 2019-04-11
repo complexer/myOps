a7b70e9247fe83fbdfddcf582b3ce04c91e26f36
ling <lingjian2016@163.com>
2019-04-11 20:37:22 +0800

    commit log浏览；工作区的单个文件上传；工作区文件内容修改更新。

--- storage/urls.py
+++ storage/urls.py
@@ -5,15 +5,16 @@ urlpatterns = [
     path('', views.repo_list, name='repo_list'),
     path('repo_create/', views.repo_create, name='repo_create'),
 
-
-    # path('repo_show/<str:repo_name>/', views.repo_show, name='repo_show'),
-    # path('file_show/<str:repo_name>/<str:hexsha>/', views.file_show, name='file_show'),
-    # path('repo_file_upload/', views.repo_file_upload, name='repo_file_upload'),
+    path('upload/', views.ws_file_upload, name='ws_file_upload'),
+    path('ws_file_update/', views.ws_file_update, name='ws_file_update'),
+    path('repo/<str:repo_name>/commit_history/', views.commit_history, name='commit_history'),
     re_path(r'^tree/(?P<repo_name>.*)/workspace$', views.ws_show, name='ws_show'),
     re_path(r'^tree/(?P<repo_name>.*)/workspace/(?P<file_path>.*)$', views.ws_file_show, name='ws_file_show'),
 
+    #master这两个考虑不用，直接展示工作区，master的文件内容直接通过下面的commit来显示和查看
     re_path(r'^tree/(?P<repo_name>.*)/master$', views.tree_show, name='tree_show'),
     re_path(r'^tree/(?P<repo_name>.*)/master/(?P<file_path>.*)$', views.tree_file_show, name='tree_file_show'),
+
     re_path(r'^tree/(?P<repo_name>.*)/(?P<commit>[0-9a-z]{40})$', views.tree_show, name='tree_last_show'),
     re_path(r'^tree/(?P<repo_name>.*)/(?P<commit>[0-9a-z]{40})/(?P<file_path>.*)$', views.tree_file_show, name='tree_last_file_show'),
 ]
--- storage/views.py
+++ storage/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 import os
 from .utils import *
 from .forms import UploadFileForm
 from .models import *
+from django.urls import reverse
 
 
 def repo_list(request):
@@ -28,51 +29,14 @@ def repo_create(request):
         context['forms'] = forms
         return render(request, 'storage/upload.html', context)
 
-def repo_show(request, repo_name):
-    repo = Repo.objects.get(name=repo_name)
-    repo_handle = RepoHandle(repo.path)
-    files, type = repo_handle.repo_search(repo_handle.head_commit.tree.hexsha)
-    context = {}
-    context['files'] = files
-    context['repo_name'] = repo_name
-    if type == "tree":
-        context['file_type'] = 'tree'
-        context['forms'] = UploadFileForm()
-        return render(request, 'storage/repo_show.html', context)
-    else:
-        raise Exception("Error Type!!!")
-
-
-def file_show(request, repo_name, hexsha):
-    repo = Repo.objects.get(name=repo_name)
-    repo_handle = RepoHandle(repo.path)
-    files, type = repo_handle.repo_search(hexsha)
-    context = {}
-    context['files'] = files
-    context['repo_name'] = repo_name
-    context['parent_dir_hexsha'] = hexsha
-    if type == "tree":
-        context['file_type'] = 'tree'
-        context['forms'] = UploadFileForm()
-        return render(request, 'storage/repo_show.html', context)
-    elif type == "blob":
-        context['file_type'] = 'blob'
-        return render(request, 'storage/repo_show.html', context)
-    else:
-        raise Exception("Error Type!!!")
-
-
-def repo_file_upload(request):
-    if request.method == "POST":
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            file_name = request.POST.get('app')
-            parent_dir_hexsha = request.POST.get('parent_dir_hexsha')
-            repo_name = request.POST.get('repo_name')
-            repo_handle = RepoHandle(Repo.objects.get(name=repo_name).path)
-
-
 def tree_show(requset, repo_name, commit=None):
+    '''
+    给commit log用展示根
+    :param requset:
+    :param repo_name:
+    :param commit:
+    :return:
+    '''
     repo = Repo.objects.get(name=repo_name)
     tree_handle = TreeHandle(repo.path, commit)
     context = {}
@@ -83,32 +47,59 @@ def tree_show(requset, repo_name, commit=None):
     return render(requset, 'storage/tree_show.html', context)
 
 def tree_file_show(request, repo_name, file_path, commit=None):
+    '''
+    给commit log用展示文件
+    :param request:
+    :param repo_name:
+    :param file_path:
+    :param commit:
+    :return:
+    '''
     repo = Repo.objects.get(name=repo_name)
     tree_handle = TreeHandle(repo.path, commit)
     context = {}
-    context['commit'] = commit
 
     if tree_handle.tree[file_path].type == 'tree':
         context['repo_name'] = repo_name
         context['trees'] = tree_handle.tree[file_path].trees
         context['blobs'] = tree_handle.tree[file_path].blobs
     else:
-        #data_stream获取数据\r\n如何在网页站示
-        context['file_content'] = tree_handle.tree[file_path].data_stream.read()
+        '''
+        data_stream获取到的数据是rb模式读取的，需要互相转换下(python中string前加r、b、u的意思需要了解下)
+        在 Python3 中，bytes 和 str 的互相转换方式是
+        str.encode('utf-8')
+        bytes.decode('utf-8')
+        '''
+        context['file_content'] = tree_handle.tree[file_path].data_stream.read().decode('utf-8')
         # context['file_content'], type = RepoHandle(repo.path).repo_search(tree_handle.tree[file_path].hexsha)
     return render(request, 'storage/tree_show.html', context)
 
 def ws_show(request, repo_name):
+    '''
+    工作区根目录展示
+    :param request:
+    :param repo_name:项目名称
+    :return:
+    '''
     repo = Repo.objects.get(name=repo_name)
     files = os.listdir(repo.path)
+    form = UploadFileForm(request.POST, request.FILES)
     context = {}
     context['files'] = files
     context['repo_name'] = repo_name
+    context['form'] = form
     return render(request, 'storage/ws_show.html', context)
 
 
 
 def ws_file_show(request, repo_name, file_path):
+    '''
+    工作区下面的文件及目录展示
+    :param request:
+    :param repo_name:
+    :param file_path:
+    :return:
+    '''
     repo = Repo.objects.get(name=repo_name)
     target_file = repo.path+file_path
     is_dir = os.path.isdir(target_file)
@@ -117,6 +108,8 @@ def ws_file_show(request, repo_name, file_path):
     context['repo_name'] = repo_name
 
     if is_dir:
+        form = UploadFileForm(request.POST, request.FILES)
+        context['form'] = form
         files = os.listdir(target_file)
         context['file_path'] = file_path +'/'
         context['files'] = files
@@ -126,5 +119,51 @@ def ws_file_show(request, repo_name, file_path):
         with open(target_file, 'r', encoding='utf-8') as f:
             context['file_content'] = f.read()
         #两种显示格式一种是文本框，一种是raw
-        # return render(request, 'storage/ws_filecontent_show.html', context)
-        return render(request, 'storage/raw.html', context)
+        return render(request, 'storage/ws_filecontent_show.html', context)
+        # return render(request, 'storage/raw.html', context)
+
+def ws_file_upload(request):
+    '''
+    文件上传
+    :param request:
+    :return:
+    '''
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            repo = Repo.objects.get(name=request.POST.get('repo_name'))
+            file = request.FILES['file']
+            file_path = request.POST.get('file_path','')
+            save_target = repo.path+file_path+file.name
+            with open(save_target, 'wb+') as destination:
+                for chunk in file.chunks():
+                    destination.write(chunk)
+            return redirect(request.POST.get('from', reverse('repo_list')))
+
+
+def commit_history(request, repo_name):
+    '''
+    展示commit log
+    :param request:
+    :param repo_name:
+    :return:
+    '''
+    repo_info = Repo.objects.get(name=repo_name)
+    repo = git.Repo(repo_info.path)
+    commit_logs = repo.iter_commits('master', max_count=10, skip=0)
+    context = {}
+    context['commit_logs'] = commit_logs
+    context['repo_name'] = repo_name
+    return render(request, 'storage/commit_log.html', context)
+
+
+def ws_file_update(request):
+    if request.method == 'POST':
+        repo_name = request.POST.get('repo_name')
+        file_path = request.POST.get('file_path')
+        file_content = request.POST.get('file_content').replace("\n","")
+        repo_info = Repo.objects.get(name=repo_name)
+        target_file = repo_info.path+file_path
+        with open(target_file, 'w', encoding='utf-8') as f:
+            f.write(file_content)
+        return redirect(request.POST.get('from', reverse('repo_list')))
