f4904bdb563bfc739b1f8debb192a4b2c76fad31
ling <lingjian2016@163.com>
2019-04-21 21:16:41 +0800

    webssh

--- myops/settings.py
+++ myops/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 REPO_DIR = 'repository/'
 REPO_URL = '/repository/'
