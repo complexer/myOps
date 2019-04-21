eed86dbe0a9936a431fe78ab107fb09915ad651a
ling <lingjian2016@163.com>
2019-04-21 21:06:56 +0800

    cmdb

--- myops/urls.py
+++ myops/urls.py
@@ -21,5 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('storage/', include('storage.urls')),
+    path('cmdb/', include('cmdb.urls')),
     path('', views.home, name='home'),
 ]
